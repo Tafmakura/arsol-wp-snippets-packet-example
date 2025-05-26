3f1747a632b1e46c249bc0afe69c9867fd2dc783
Taf Makura <tafmakura@gmail.com>
2025-05-26 22:32:39 +0200

    Update functions-php-snippets-loader.php

--- functions/functions-php-snippets-loader.php
+++ functions/functions-php-snippets-loader.php
@@ -67,7 +67,7 @@ function add_admin_functions($php_options) {
 
     $php_options[] = array(
         'name' => 'Example Admin Functions XXXX',
-        'loading_order' => 6,
+        'loading_order' => 66,
         'file' => __DIR__ . '/../snippets/php/example.php'
     );
     return $php_options;
